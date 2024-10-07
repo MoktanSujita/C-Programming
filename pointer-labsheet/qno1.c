bc8813ad412bf02c998df49db95789dc514e1db1
Moktan Sujita <sujitamoktan77@gmail.com>
2024-10-07 21:54:54 +0545

    Commit

---
+++ pointer-labsheet/qno1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a; //variable initialization
+    int *pa=&a;//pointer to integer
+    char c;//variable initialization
+    char *pc=&c;//pointer to char
+    float f; //variable initialization
+    float *pf=&f;////pointer to float
+    printf("Memory address \na:%d\t c:%d\t f:%d",*pa,*pc,*pf);
+    return 0;
+}
