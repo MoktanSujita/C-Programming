659e554e4d7b1e77f22fac943d221099f89be5e2
Moktan Sujita <sujitamoktan77@gmail.com>
2024-10-07 22:04:18 +0545

    Commit

---
+++ pointer-labsheet/qno2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{ 
+    int n;
+    printf("Enter the no of array elements:");
+    scanf("%d",&n);
+    int arr[n],sum=0;//variable declaration
+    int *parr=&arr;//pointer to array
+    printf("Enter the array elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=*(parr+i);//finding sum using pointer
+    }
+    printf("The sum of the elements is %d",sum);
+    return 0;
+
+}
