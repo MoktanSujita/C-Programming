2870f701f7483f172e80e3ba0a4c0091bfe74208
Moktan Sujita <sujitamoktan77@gmail.com>
2024-10-09 22:20:49 +0545

    commit

---
+++ pointer-labsheet/qno5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a=5;
+    int *pa=&a;
+    int s=6;
+    int *ps=&s;
+    printf("The values before swapping \n a:%d \t s:%d",a,s);
+    int t=*pa;
+    a=*ps;
+    s=t;
+    printf("\nThe values after swapping value \n a:%d\t s:%d",a,s);
+    return 0;
+
+}
