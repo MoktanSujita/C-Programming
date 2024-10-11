4d4fdcfb52057224ca07ca9cdc2a9943904e07af
Moktan Sujita <sujitamoktan77@gmail.com>
2024-10-11 21:13:49 +0545

    commit

--- pointer-labsheet/qno4.c
+++ pointer-labsheet/qno4.c
@@ -1,6 +1,20 @@
+//WAP to print the size of pointers of different data types 
 #include <stdio.h>
 #include <conio.h>
 int main()
 {
+    //defining variables
+    int a;
+    float b;
+    char c;
+    //declaring pointer variables
+    int *pa=&a;
+    float *pb=&b;
+    char *pc=&c;
+ //using sizeof() function and printing the size of pointers
+    printf("Size of pointer int:%d\n",sizeof(pa));
+    printf("Size of pointer float:%d\n",sizeof(pb));
+    printf("Size of pointer char:%d\n",sizeof(pc));
+    return 0;
     
 }
